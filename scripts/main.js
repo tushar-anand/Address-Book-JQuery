9d02e01ec89717f0ffbc450cb66ace2fa60b99eb
Tushar <tusharanandnet@gmail.com>
2020-10-21 11:18:34 +0530

    1. Cards are being created. 2. Search using Filters() now functional. 3. Still trying to figure out how to bind the dynamically created DOM Elements with the document.

--- scripts/main.js
+++ scripts/main.js
@@ -4,41 +4,95 @@ $(document).ready(function () {
     var contacts = [];
     var id = 0;
 
+    //Clearing the modal after closing the form
+    $(".modal").on("hidden.bs.modal", function () {
+        $(this).find('form').trigger("reset");
+    });
+
     //Captuing the form input
-    $("#saveNewContact").click(function () {
+    $("#saveNewContact").on("click", function () {
 
         var newFormDetails = {};
         newFormDetails["id"] = id;
-        id++;
         $.each($("#create-new-contact").serializeArray(), function (i, field) {
+            
             newFormDetails[field.name] = field.value;
+            console.log(newFormDetails);
         });
+        contacts[id] = newFormDetails;
+        id++;
 
-        console.log(newFormDetails["firstname"]);
+        // Display no records found
+        if (contacts.length != 0) {
+            $("#noRecords").css("display", "none");
+        }
 
-        jQuery("<div/>", {
+        var mouseHoverEnter = function(){
+            $(".actionButtons").css("background-color", "red");
+        };
+    
+        var mouseHoverExit = function(){
+            $(".actionButtons").toggle(false);
+        };
+    
+        $(function() {
+            $(".contact-card").hover(mouseHoverEnter, mouseHoverExit);
+        });
+    
+
+
+        // After completion of every feature, edit the HTML card to be hidden without any text present. Then for every new creation of the card, clone the html and provide value to it.
+        //https://www.w3schools.com/jquery/jquery_dom_add.asp
+        
+
+        // Adding a new card
+        $("<div/>", {
             class: "contact-card",
             id: newFormDetails["id"],
-        }).appendTo(".cards-flex-container");
+        }).appendTo(".cards-flex-container").hover(mouseHoverExit, mouseHoverEnter);
 
-        jQuery("<div/>", {
+        $("<div/>", {
             class: "profile-picture-container",
         }).appendTo(".contact-card:last");
 
-        jQuery("<img/>", {
+        //Adding card image
+        $("<img/>", {
             class: "rounded-circle profile-picture",
             src: "images/profile-picture.png",
             alt: "profile picture",
         }).appendTo(".profile-picture-container:last");
 
-        jQuery("<div/>", {
+        //Adding card details from here
+        $("<div/>", {
             class: "profile-details",
         }).appendTo(".contact-card:last");
 
-        jQuery("<h4/>", {
-            text: newFormDetails["firstname"] + " " + newFormDetails["lastname"],
+
+        //Adding card title
+        $("<div/>", {
+            class: "card-title",
         }).appendTo(".profile-details:last");
 
+        //Adding user name
+        $("<h4/>", {
+            text: newFormDetails["firstname"] + " " + newFormDetails["lastname"],
+        }).appendTo(".card-title:last");
+
+        //Adding action buttons
+        $('<div />', {
+            class: "actionButtons",
+            html: '<i class="fa fa-eye"></i><i class="fa fa-pencil"></i><i class="fa fa-trash"></i>',
+        }).appendTo(".card-title:last");
+
+        // $("</>", {
+        //     class: "actionButtons",
+        //     html: actionIcons,
+        // }).appendTo(".card-title:last");
+
+        //Adding the <HR> tag
+        $("<hr>").appendTo(".profile-details:last");
+
+        // Adding Phone number
         var phoneTitle = $('<span />', {
             html: '<b>Phone: </b>',
         });
@@ -47,7 +101,7 @@ $(document).ready(function () {
             html: newFormDetails["phone"],
         });
 
-        jQuery("<div/>", {
+        $("<div/>", {
             class: "card-phone-number",
             html: phoneTitle,
         }).appendTo(".profile-details:last");
@@ -63,7 +117,7 @@ $(document).ready(function () {
             html: newFormDetails["email"],
         });
 
-        jQuery("<div/>", {
+        $("<div/>", {
             class: "card-email",
             html: emailTitle,
         }).appendTo(".profile-details:last");
@@ -79,7 +133,7 @@ $(document).ready(function () {
             html: newFormDetails["jobtitle"],
         });
 
-        jQuery("<div/>", {
+        $("<div/>", {
             class: "card-job-title",
             html: jobTitle,
         }).appendTo(".profile-details:last");
@@ -95,7 +149,7 @@ $(document).ready(function () {
             html: newFormDetails["companyname"],
         });
 
-        jQuery("<div/>", {
+        $("<div/>", {
             class: "card-company",
             html: companyTitle,
         }).appendTo(".profile-details:last");
@@ -111,7 +165,7 @@ $(document).ready(function () {
             html: newFormDetails["addressLineOne"] + " " + newFormDetails["addressLineTwo"],
         });
 
-        jQuery("<div/>", {
+        $("<div/>", {
             class: "card-address",
             html: addressTitle,
         }).appendTo(".profile-details:last");
@@ -119,113 +173,40 @@ $(document).ready(function () {
         addressValue.appendTo(".card-address:last");
     });
 
-    //Clearing the modal after closing the form
-    $(".modal").on("hidden.bs.modal", function () {
-        $(this).find('form').trigger("reset");
+    // Display card details on click
+    // $(".contact-card").hover(function () {
+    //     $(this).css("background-color", "yellow");
+    // }, function () {
+    //     $(this).css("background-color", "pink");
+    // });
+
+    // $(".contact-card").on({
+    //     mouseenter: function () {
+    //         $(this).css("background-color", "yellow");
+    //     },
+
+    //     mouseleave: function () {
+    //         $(this).css("background-color", "white");
+    //     },
+    //     click: function () {
+    //         $(this).css("background-color", "green");
+    //     },
+    // });
+
+
+
+
+
+    $(".cards-flex-container div").on("mouseenter", ".cotact-card", function(){
+        $(this).css("background-color", "yellow");
+
     });
 
-    //Preventing the modal from closing when clicked outside the modal or when pressed the "ESC" keyboard button
-
-    $(".contact-card").click(function () {
-        jQuery("<div/>", {
-            class: "contact-card",
-        }).appendTo(".cards-flex-container");
-
-        jQuery("<div/>", {
-            class: "profile-picture-container",
-        }).appendTo(".contact-card:last");
-
-        jQuery("<img/>", {
-            class: "rounded-circle profile-picture",
-            src: "images/profile-picture.png",
-            alt: "profile picture",
-        }).appendTo(".profile-picture-container:last");
-
-        jQuery("<div/>", {
-            class: "profile-details",
-        }).appendTo(".contact-card:last");
-
-        jQuery("<h4/>", {
-            text: "Tushar Anand",
-        }).appendTo(".profile-details:last");
-
-        var phoneTitle = $('<span />', {
-            html: '<b>Phone: </b>'
+    // Search Box
+    $("#searchbox").on("keyup", function () {
+        var a = $(this).val().toLowerCase();
+        $(".contact-card").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(a) > -1)
         });
-
-        var phoneValue = $('<span />', {
-            html: '+91 8789067614'
-        });
-
-        jQuery("<div/>", {
-            class: "card-phone-number",
-            html: phoneTitle,
-        }).appendTo(".profile-details:last");
-
-        phoneValue.appendTo(".card-phone-number:last");
-
-        // Adding Email
-        var emailTitle = $('<span />', {
-            html: '<b>Email: </b>',
-        });
-
-        var emailValue = $('<span />', {
-            html: 'tushar@test.com',
-        });
-
-        jQuery("<div/>", {
-            class: "card-email",
-            html: emailTitle,
-        }).appendTo(".profile-details:last");
-
-        emailValue.appendTo(".card-email:last");
-
-        // Adding Job Title
-        var jobTitle = $('<span />', {
-            html: '<b>Job Title: </b>',
-        });
-
-        var jobTitleValue = $('<span />', {
-            html: 'Software Engineer',
-        });
-
-        jQuery("<div/>", {
-            class: "card-job-title",
-            html: jobTitle,
-        }).appendTo(".profile-details:last");
-
-        jobTitleValue.appendTo(".card-job-title:last");
-
-        // Adding Company
-        var companyTitle = $('<span />', {
-            html: '<b>Company: </b>',
-        });
-
-        var companyValue = $('<span />', {
-            html: 'Nowcom Global Services',
-        });
-
-        jQuery("<div/>", {
-            class: "card-company",
-            html: companyTitle,
-        }).appendTo(".profile-details:last");
-
-        companyValue.appendTo(".card-company:last");
-
-        // Adding Address
-        var addressTitle = $('<span />', {
-            html: '<b>Address: </b>',
-        });
-
-        var addressValue = $('<span />', {
-            html: 'Mountain View, California, United States',
-        });
-
-        jQuery("<div/>", {
-            class: "card-address",
-        }).appendTo(".profile-details:last");
-
-        companyValue.appendTo(".card-address:last");
-
     });
 });
